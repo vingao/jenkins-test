999f26b061caf334cb9a75437f0e6ade4c641343
Vincent Gao <vingao@gmail.com>
2018-02-10 23:35:34 -0500

    adds Jacoco plugin add a test

--- src/test/java/com/fidsafe/BankAccountTest.java
+++ src/test/java/com/fidsafe/BankAccountTest.java
@@ -1,7 +1,16 @@
 package com.fidsafe;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    @Test
+    public void debitWithSufficentFunds() {
+        BankAccount account = new BankAccount(10);
+        double amount = account.debit(5);
+        Assert.assertEquals(5.0, amount);
+    }
 }
