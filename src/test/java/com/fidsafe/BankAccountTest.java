a70334f6a7edfccf1ae1b2f90a0ea5c8c5efe946
Vincent Gao <vingao@gmail.com>
2018-02-11 00:03:10 -0500

    Tests debit() with insufficent funds

--- src/test/java/com/fidsafe/BankAccountTest.java
+++ src/test/java/com/fidsafe/BankAccountTest.java
@@ -13,4 +13,11 @@ public class BankAccountTest {
         double amount = account.debit(5);
         Assert.assertEquals(5.0, amount, 0.0001);
     }
+
+    @Test
+    public void debitWithInsufficentFunds() {
+        BankAccount account = new BankAccount(10);
+        double amount = account.debit(11);
+        Assert.assertEquals(10.0, amount, 0.0001);
+    }
 }
